fc3356809795aa1f868b01e888e8af15e814eff4
Edson Moreno <edsonmorenoruiz@gmail.com>
2020-02-19 16:15:56 -0600

    testing mset

--- red.py
+++ red.py
@@ -1,6 +1,11 @@
 import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-r.set('edson', 'test')
+
+r.set('events', 'test')
 e = r.get('edson')
 print(e.decode('ASCII'))
+
+r.mset({"Mexico": "CDMX", "Croatia": "Zagreb"})
+c = r.get("Croatia")
+print(c.decode('ASCII'))
