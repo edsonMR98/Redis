6a6df7b7d2f49fe7c07c428633012826024d226f
Edson Moreno <edsonmorenoruiz@gmail.com>
2020-02-19 16:54:02 -0600

    List converted to dict

--- red.py
+++ red.py
@@ -1,4 +1,5 @@
 import redis
+import json
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -13,9 +14,11 @@ print(c.decode('ASCII'))
 # Redis lists
 r.rpush('eventsNotPersisted', str({"question": 1, "prop": 1, "user": 1}))
 r.rpush('eventsNotPersisted', str({"question": 2, "prop": 2, "user": 2}))
-r.rpush('eventsNotPersisted', str({"question": 3, "prop": 3, "user": 3}))
+r.rpush('eventsNotPersisted', str({'question': 3, 'prop': 3, 'user': 3}))
 
 print("List len:", r.llen('eventsNotPersisted'))
 for event in range(0, r.llen('eventsNotPersisted')):
     e = r.lindex('eventsNotPersisted', event)
-    print(e.decode('ASCII'))
+    eStr = e.decode('ASCII')
+    eDict = json.loads(e.decode('ASCII').replace("'", '"'))
+    print("Question", eDict["question"])
